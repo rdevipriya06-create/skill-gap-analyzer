8dd5b17ee633df2f70422a22c26910d8cff37887
Devi Priya <rdevipriya06@gmail.com>
2026-04-04 17:44:19 +0530

    Final fix: Use Cloud PORT and detailed logs

--- server.js
+++ server.js
@@ -464,6 +464,6 @@ app.get("/courses/:skill", (req, res) => {
 });
 
 /* ---------------- SERVER ---------------- */
-app.listen(5000, () => {
-    console.log("Server running on http://localhost:5000");
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}`);
 });
